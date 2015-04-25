1d1ba9078e14a0ac0180548f9d7170ee487d0bbd
Andy Arvanitis <andyarvanitis@gmail.com>
2015-04-25 11:07:59 -0700

    Re-arrange C++ headers

--- include/purescript.hh
+++ include/purescript.hh
@@ -4,75 +4,29 @@
 // Standard includes
 //
 #include <functional>
-#include <memory>
 #include <string>
-#include <iostream>
 #include <stdexcept>
 #include "bind.hh"
-#include "shared_list.hh"
+#include "memory.hh"
 #include "any_map.hh"
+#include "shared_list.hh"
 
 namespace PureScript {
-
-// Type aliases
 
 template <typename A, typename B>
 using fn = std::function<B(A)>;
 
-template <typename T>
-using managed = std::shared_ptr<T>;
+template <typename B>
+using eff_fn = std::function<B()>;
+
+using string = std::string;
+
+using runtime_error = std::runtime_error;
 
 template <typename T>
 using list = shared_list<T>;
 
 using list_index_type = list<void*>::size_type;
-
-using string = std::string;
-
-template <typename B> using eff_fn = std::function<B()>;
-using runtime_error = std::runtime_error;
-
-// Function aliases
-
-template <typename T, typename... ArgTypes>
-auto construct(ArgTypes... args) ->
-    typename std::enable_if<std::is_assignable<std::shared_ptr<void>,T>::value,T>::type {
-  return std::make_shared<typename T::element_type>(args...);
-}
-
-template <typename T, typename... ArgTypes>
-constexpr auto construct(ArgTypes... args) ->
-    typename std::enable_if<!std::is_assignable<std::shared_ptr<void>,T>::value,std::shared_ptr<T>>::type {
-  return std::make_shared<T>(args...);
-}
-
-template <typename Ctor, typename... CArgs, typename... Args>
-constexpr auto constructor(Args&&... args) ->
-    decltype(Private::Bind<sizeof...(CArgs) - sizeof...(Args)>::bind(construct<Ctor, CArgs...>,std::forward<Args>(args)...)) {
-  return Private::Bind<sizeof...(CArgs) - sizeof...(Args)>::bind(construct<Ctor, CArgs...>, std::forward<Args>(args)...);
-}
-
-template <typename T, typename U>
-constexpr auto instance_of(const std::shared_ptr<U>& a) ->
-    typename std::enable_if<std::is_assignable<std::shared_ptr<void>,T>::value,T>::type {
-  return std::dynamic_pointer_cast<typename T::element_type>(a);
-}
-
-template <typename T, typename U>
-constexpr auto instance_of(const std::shared_ptr<U> a) ->
-  typename std::enable_if<!std::is_assignable<std::shared_ptr<void>,T>::value,std::shared_ptr<T>>::type {
-  return std::dynamic_pointer_cast<T>(a);
-}
-
-template <typename T, typename U>
-constexpr auto get(const std::shared_ptr<U>& a) -> typename T::element_type {
-  return *std::dynamic_pointer_cast<typename T::element_type>(a);
-}
-
-template <typename U>
-constexpr auto get(const std::shared_ptr<U>& a) -> U {
-  return *std::dynamic_pointer_cast<U>(a);
-}
 
 } // namespace PureScript
 
