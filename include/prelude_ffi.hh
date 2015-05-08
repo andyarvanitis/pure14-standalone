0e83e1cee5e24f843ea6f2360e394b8c11536c6e
Andy Arvanitis <andyarvanitis@gmail.com>
2015-05-07 18:12:04 -0700

    Add module #include #ifdef protectors

--- include/prelude_ffi.hh
+++ include/prelude_ffi.hh
@@ -8,151 +8,160 @@
 
 namespace Prelude {
 
-    using namespace PureScript;
+  using namespace PureScript;
 
-	inline auto showStringImpl(string s) -> string {
-		return s;
-	}
+  inline auto showStringImpl(string s) -> string {
+    return s;
+  }
 
-	inline auto showNumberImpl(double x) -> string {
-		return std::to_string(x);
-	}
+  inline auto showNumberImpl(double x) -> string {
+    return std::to_string(x);
+  }
 
-	inline auto showIntImpl(long x) -> string {
-		return std::to_string(x);
-	}
+  inline auto showIntImpl(long x) -> string {
+    return std::to_string(x);
+  }
 
-	inline auto showCharImpl(char x) -> string {
-		return std::to_string(x);
-	}
+  inline auto showCharImpl(char x) -> string {
+    return std::to_string(x);
+  }
 
-    template <typename A>
-	inline auto showArrayImpl(fn<A,list<A>> f) -> fn<list<A>,string> {
-		return [=](list<A> a) {
-		    return "";
-		};
-	}
+  template <typename A>
+  inline auto showArrayImpl(fn<A,list<A>> f) -> fn<list<A>,string> {
+    return [=](list<A> a) {
+      return "";
+    };
+  }
 
-	inline auto numAdd(double x) -> fn<double,double> {
-		return [=](double y) {
-			return x + y;
-		};
-	}
+  inline auto numAdd(double x) -> fn<double,double> {
+    return [=](double y) {
+      return x + y;
+    };
+  }
 
-	inline auto numSub(double x) -> fn<double,double> {
-		return [=](double y) {
-			return x - y;
-		};
-	}
+  inline auto numSub(double x) -> fn<double,double> {
+    return [=](double y) {
+      return x - y;
+    };
+  }
 
-	inline auto numMul(double x) -> fn<double,double> {
-		return [=](double y) {
-			return x * y;
-		};
-	}
+  inline auto numMul(double x) -> fn<double,double> {
+    return [=](double y) {
+      return x * y;
+    };
+  }
 
-	inline auto numDiv(double x) -> fn<double,double> {
-		return [=](double y) {
-			return x / y;
-		};
-	}
+  inline auto numDiv(double x) -> fn<double,double> {
+    return [=](double y) {
+      return x / y;
+    };
+  }
 
-	inline auto intAdd(long x) -> fn<long,long> {
-		return [=](long y) {
-			return x + y;
-		};
-	}
+  inline auto intAdd(long x) -> fn<long,long> {
+    return [=](long y) {
+      return x + y;
+    };
+  }
 
-	inline auto intSub(long x) -> fn<long,long> {
-		return [=](long y) {
-			return x - y;
-		};
-	}
+  inline auto intSub(long x) -> fn<long,long> {
+    return [=](long y) {
+      return x - y;
+    };
+  }
 
-	inline auto intMul(long x) -> fn<long,long> {
-		return [=](long y) {
-			return x * y;
-		};
-	}
+  inline auto intMul(long x) -> fn<long,long> {
+    return [=](long y) {
+      return x * y;
+    };
+  }
 
-	inline auto intDiv(long x) -> fn<long,long> {
-		return [=](long y) {
-			return x / y;
-		};
-	}
+  inline auto intDiv(long x) -> fn<long,long> {
+    return [=](long y) {
+      return x / y;
+    };
+  }
 
-	inline auto intMod(long x) -> fn<long,long> {
-		return [=](long y) {
-			return x % y;
-		};
-	}
+  inline auto intMod(long x) -> fn<long,long> {
+    return [=](long y) {
+      return x % y;
+    };
+  }
 
-	inline auto concatString(string x) -> fn<string,string> {
-		return [=](string y) {
-			return x + y;
-		};
-	}
+  inline auto concatString(string x) -> fn<string,string> {
+    return [=](string y) {
+      return x + y;
+    };
+  }
 
-	template <typename T>
-	inline auto refEq(T ref1) -> fn<T,bool> {
-		return [=](T ref2) {
-			return ref1 == ref2;
-		};
-	}
+  template <typename T>
+  inline auto refEq(T ref1) -> fn<T,bool> {
+    return [=](T ref2) {
+      return ref1 == ref2;
+    };
+  }
 
-    template <typename A>
-	inline auto eqArrayImpl(fn<A,fn<A,bool>> f) -> fn<list<A>,fn<list<A>,bool>> {
-        return [=](list<A> xs) {
-            return [=](list<A> ys) {
-                return false;
-            };
-		};
-	}
+  template <typename A>
+  inline auto eqArrayImpl(fn<A,fn<A,bool>> f) -> fn<list<A>,fn<list<A>,bool>> {
+    return [=](list<A> xs) {
+      return [=](list<A> ys) {
+        return false;
+      };
+    };
+  }
 
-	inline auto boolAnd(bool x) -> fn<bool,bool> {
-		return [=](bool y) {
-			return x && y;
-		};
-	}
+  inline auto boolAnd(bool x) -> fn<bool,bool> {
+    return [=](bool y) {
+      return x && y;
+    };
+  }
 
-	inline auto boolOr(bool x) -> fn<bool,bool> {
-		return [=](bool y) {
-			return x || y;
-		};
-	}
+  inline auto boolOr(bool x) -> fn<bool,bool> {
+    return [=](bool y) {
+      return x || y;
+    };
+  }
 
-	inline auto boolNot(bool x) -> bool {
-		return !x;
-	}
+  inline auto boolNot(bool x) -> bool {
+    return !x;
+  }
 
-    template <typename A>
-    inline auto unsafeCompareImpl(A arg) -> fn<A,std::shared_ptr<void>> {
-        // return unsafeCompareImpl<A><A>(construct<LT_>())(construct<EQ_>())(construct<GT_>())(arg);
-		return [=](A) {
-			return nullptr;
-		};
-    }
+  template <typename A>
+  inline auto unsafeCompareImpl(A arg) -> fn<A,std::shared_ptr<void>> {
+    return [=](A) {
+      return nullptr;
+    };
+  }
 
-    template <typename A>
-    inline auto concatArray(list<A> a) -> fn<list<A>,list<A>> {
-        return [=](list<A> b) {
-            return a.append(b);
-        };
-    }
+  template <typename A>
+  inline auto concatArray(list<A> a) -> fn<list<A>,list<A>> {
+    return [=](list<A> b) {
+      return a.append(b);
+    };
+  }
 
-    template <typename A, typename B>
-    inline auto arrayMap(fn<A,B> f) -> fn<list<A>,list<B>> {
-        return [=](list<A> a) {
-            return list<B>();
-        };
-    }
+  template <typename A, typename B>
+  inline auto arrayMap(fn<A,B> f) -> fn<list<A>,list<B>> {
+    return [=](list<A> a) {
+      return list<B>();
+    };
+  }
 
-    template <typename A, typename B>
-    inline auto arrayBind(list<A> a) -> fn<fn<A,list<B>>,list<B>> {
-        return [=](fn<A,list<B>> f) {
-            return list<B>();
-        };
-    }
+  template <typename A, typename B>
+  inline auto arrayBind(list<A> a) -> fn<fn<A,list<B>>,list<B>> {
+    return [=](fn<A,list<B>> f) {
+      return list<B>();
+    };
+  }
+
+  template <typename A>
+  inline auto ordArrayImpl(fn<A,fn<A,int>> f) -> fn<list<A>,fn<list<A>,int>> {
+    return [=](list<A> xs) {
+      return [=](list<A> ys) {
+        return 0;
+      };
+    };
+  }
+
 }
 
 
