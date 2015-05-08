607440580b14790d379f63aa55079378fc291fe2
Andy Arvanitis <andyarvanitis@gmail.com>
2015-05-07 22:54:14 -0700

    Add Control.Monad.Eff FFI stubs

--- include/prelude_ffi.hh
+++ include/prelude_ffi.hh
@@ -170,7 +170,27 @@ namespace Prelude {
       };
     };
   }
+}
 
+namespace Control_Monad_Eff {
+
+  using namespace PureScript;
+
+  template <typename A, typename B>
+  inline auto bindE(eff_fn<A> a) -> fn<fn<A,eff_fn<B>>,eff_fn<B>> {
+    return [=](fn<A,eff_fn<B>> f) {
+      return [=]() {
+        return f(a())();
+      };
+    };
+  }
+
+  template <typename A>
+  inline auto returnE(A a) -> eff_fn<A> {
+    return [=]() {
+      return a;
+    };
+  }
 }
 
 
